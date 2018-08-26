75d88097ad2001126f424989c2a3b411d9ffb9de
dambusm <willschaller@gmail.com>
2018-08-26 23:30:37 +0100

    Remove users route, add error handling routes

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ code: 'unexpected' });
+  }
+});
 
 module.exports = app;
